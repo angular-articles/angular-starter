2249de5e7e66cafd4c58e0b836c62f9383d17ca1
Yury <yury.kopyl@gmail.com>
2019-07-30 12:54:29 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,17 +6,17 @@ import {IsNotLoggedGuard} from './shared/guards/isNotLogged.guard';
 import {NotFountComponent} from './not-fount/not-fount.component';
 
 const routes: Routes = [
-	{path: '', redirectTo: '/home', pathMatch: 'full'},
-	{path: 'login', loadChildren: './login/login.module#LoginModule', canLoad: [IsNotLoggedGuard]},
-	{path: 'signup', loadChildren: './signup/signup.module#SignupModule', canLoad: [IsNotLoggedGuard]},
-	{path: 'home', loadChildren: './core/core.module#CoreModule', canLoad: [IsLoggedGuard]},
-	{path: '404', component: NotFountComponent},
-	{path: '**', redirectTo: '/404'}
+    {path: '', redirectTo: '/home', pathMatch: 'full'},
+    {path: 'login', loadChildren: './login/login.module#LoginModule', canLoad: [IsNotLoggedGuard], data: {animations: 'HomePage'}},
+    {path: 'signup', loadChildren: './signup/signup.module#SignupModule', canLoad: [IsNotLoggedGuard], data: {animations: 'AboutPage'}},
+    {path: 'home', loadChildren: './core/core.module#CoreModule', canLoad: [IsLoggedGuard]},
+    {path: '404', component: NotFountComponent},
+    {path: '**', redirectTo: '/404'}
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes)],
-	exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 
 export class AppRoutingModule {
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,16 @@
 import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+
+import {slide} from './shared/animations/slide';
 
 @Component({
-	selector: 'app-root',
-	templateUrl: './app.component.html'
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    animations: [slide]
 })
 
 export class AppComponent {
+    prepareRoute(outlet: RouterOutlet) {
+        return outlet && outlet.activatedRouteData && outlet.activatedRouteData.animation;
+    }
 }
